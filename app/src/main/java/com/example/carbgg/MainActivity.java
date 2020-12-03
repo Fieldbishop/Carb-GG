111c4b7e02426561822576af823839e3f62c86ee
Fieldbishop <lpiiska@gmail.com>
2020-12-03 17:52:51 +0200

    lassi20.12.3

--- app/src/main/java/com/example/carbgg/MainActivity.java
+++ app/src/main/java/com/example/carbgg/MainActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    float x1, x2, y1, y2;                                                   // values needed for swipe
+    float x1, x2, y1, y2;                                                   // values for swipe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,26 @@ public class MainActivity extends AppCompatActivity {
                 if (x1 < x2) {
                     Intent i = new Intent(MainActivity.this, SwipeLeft.class);
                     startActivity(i);
+                    overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
                 }else if(x2 < x1){
                     Intent i = new Intent(MainActivity.this, SwipeRight.class);
                     startActivity(i);
+                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
                 }
                 break;
         }
         return false;
+    }
+    public void btnSettings(View view) {
+        Intent j = new Intent(MainActivity.this,SettingsPage.class);
+        startActivity(j);
+    }
+    public void btnMeals(View view){
+        Intent j = new Intent(MainActivity.this,SwipeLeft.class);
+        startActivity(j);
+    }
+    public void btnHistory(View view){
+        Intent j = new Intent(MainActivity.this,SwipeRight.class);
+        startActivity(j);
     }
 }
