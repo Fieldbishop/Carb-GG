778ecca450efea119192d18988ab8ee888a303bb
54731 <santericorp@gmail.com>
2020-12-09 20:42:46 +0200

    add javadoc for SettingsPage-activity

--- app/src/main/java/com/example/carbgg/SettingsPage.java
+++ app/src/main/java/com/example/carbgg/SettingsPage.java
@@ -10,12 +10,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+/**
+ * Activity for setting insulin coefficient per 10g of carbohydrates and storing it as a preference
+ */
 public class SettingsPage extends AppCompatActivity {
     private EditText et;
     public final String savefile = "configs";
     public final String insulinEfficiencyKey = "inEfKey";
     protected float insulinEfficiency;
 
+    /**
+     *Creates the page for the activity and assigns insulin coefficiency display and
+     * reads preferences to display the value, sets displays for home arrow
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,10 @@ public class SettingsPage extends AppCompatActivity {
         float insulinEf = prefGet.getFloat("inEfKey", 1);
         et.setText(Float.toString(insulinEf));
     }
+
+    /**
+     *Gets data from the EditText field and saves it into a shared preference slot
+     */
     public void btnSaveCoefficient(View view) {
         insulinEfficiency = Float.parseFloat(et.getText().toString());
         SharedPreferences prefPutter = getSharedPreferences(savefile,Context.MODE_PRIVATE);
@@ -37,6 +48,10 @@ public class SettingsPage extends AppCompatActivity {
         Intent j = new Intent(SettingsPage.this,MainActivity.class);
         startActivity(j);
     }
+
+    /**
+     * Home arrow to finish settings activity
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home) {
