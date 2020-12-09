39fec8b1a69ab347b951f04c0abd22fdd82bcd08
Fieldbishop <lpiiska@gmail.com>
2020-12-09 20:51:42 +0200

    javadoc datapoints listtosend and singletonclasshistory

--- app/src/main/java/com/example/carbgg/DataPointToSave.java
+++ app/src/main/java/com/example/carbgg/DataPointToSave.java
@@ -1,23 +1,42 @@
 package com.example.carbgg;
 
+/**
+ * class for creating objects for singleton arraylist "SingletonClassHistory"
+ */
 public class DataPointToSave {
     private Float calculatedCarbs;
     private String dateOfCarbs;
 
-
+    /**
+     * constructor
+     * @param dateOfCarbs date of taking the datapoint
+     * @param calculatedCarbs combined carbohydrate intake of a calculation
+     */
     public DataPointToSave(String dateOfCarbs,float calculatedCarbs) {
         this.dateOfCarbs = dateOfCarbs;
         this.calculatedCarbs = calculatedCarbs;
     }
 
+    /**
+     * getCarbs
+     * @return Carbs
+     */
     public Float getCarbs() {
         return calculatedCarbs;
     }
 
+    /**
+     * getTime
+     * @return Time
+     */
     public String getTime() {
         return dateOfCarbs;
     }
 
+    /**
+     * toString method
+     * @return carbs, date in string format
+     */
     @Override
     public String toString() {
         return calculatedCarbs + ", " + dateOfCarbs;
--- app/src/main/java/com/example/carbgg/SingletonClassHistory.java
+++ app/src/main/java/com/example/carbgg/SingletonClassHistory.java
@@ -2,10 +2,17 @@ package com.example.carbgg;
 
 import java.util.ArrayList;
 
+/**
+ * Singleton arraylist for storing objects "DataPointToSave"
+ */
 public class SingletonClassHistory {
     private static ArrayList<DataPointToSave> dataPoints;
     private static SingletonClassHistory ourInstance = new SingletonClassHistory();
 
+    /**
+     * getInstance
+     * @return instance
+     */
     public static SingletonClassHistory getInstance() {
         if(ourInstance==null)
             ourInstance=new SingletonClassHistory();
@@ -13,23 +20,43 @@ public class SingletonClassHistory {
         return ourInstance;
     }
 
+    /**
+     * Creates a new Arraylist if one is not found
+     */
     private SingletonClassHistory() {
         dataPoints = new ArrayList<DataPointToSave>();
     }
 
+    /**
+     * adds an object DataPointToSave on to the list
+     * @param date date of calculation
+     * @param carbs total carbs calculated
+     */
     public void addNew(String date, float carbs) {
         dataPoints.add(new DataPointToSave(date, carbs));
     }
 
+    /**
+     *
+     * @return arraylist
+     */
     public ArrayList<DataPointToSave> getAll() {
         return dataPoints;
     }
 
+    /**
+     * removes an object in arraylist with position "i" provided
+     * @param i the index of the item we want to remove
+     * @return arraylist with the object removed
+     */
     public ArrayList<DataPointToSave> removeOne(int i){
         dataPoints.remove(i);
         return dataPoints;
     }
 
+    /**
+     * Clears list (ununsed)
+     */
     public void eraseList() {
         ourInstance = null;
     }
